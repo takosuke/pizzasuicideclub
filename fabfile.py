90ee71d5644d64e43a8c3d8fd8aad915fb4b8658
Mario Campos <mariocampos0001@gmail.com>
2013-03-26 20:11:43 +0100

    fabfile and  requirements moddified

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,18 @@
-from fabric.api import *
- 
-# Configure user, private key, etc. for SFTP deployment
-env.user = 'takosuke'
-env.hosts = ['31.222.155.138']
-env.keyfile = ['$HOME/.ssh/id_rsa']
- 
-def pack():
-    local('python setup.py sdist --formats=gztar', capture=False)
- 
+
+from fabric.api import env, run, cd
+
+USERNAME = 'takosuke'
+SERVER = '31.222.155.138'
+APP_NAME = 'psc_app'
+PROJECT_DIR = '/home/takosuke/public_html/pizzasuicideclub.com/public/'
+WSGI_SCRIPT = '__init__.py'
+
+env.hosts = ["%s@%s" % (USERNAME, SERVER)]
+
 def deploy():
-    dist = local('python setup.py --fullname', capture=True).strip()
-    put('dist/%s.tar.gz' % dist, '/tmp/psc.tar.gz')
- 
-    # now we're on the remote host from here on out!
-    run('mkdir /tmp/psc')
-    with cd('/tmp/psc'):
-        run('tar zxf /tmp/psc.tar.gz')
-        run('mv ' + dist + '/* .')
-        sudo('/home/takosuke/public_html/pizzasuicideclub.com/public/flask_PSC/bin/python setup.py install')
- 
-    sudo('chown -R nginx:nginx /home/takosuke/public_html/pizzasuicideclub.com/public/')
-    sudo('rm -rf /tmp/psc /tmp/psc.tar.gz')
- 
-    # alert uWSGI to reload the project
-    sudo('touch /home/takosuke/public_html/pizzasuicideclub.com/public/psc_app/__init__.py')
+    with cd(PROJECT_DIR):
+        run('git pull')
+        run('bin source/activate')
+        run('pip install -r requirements.txt')
+        run('touch %s' % WSGI_SCRIPT)
+
