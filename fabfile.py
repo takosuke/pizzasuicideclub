e624512f252088ec312027e03de51ba647b2c7e0
Mario Campos <mariocampos0001@gmail.com>
2013-03-26 20:15:44 +0100

    fabfile moddified

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ env.hosts = ["%s@%s" % (USERNAME, SERVER)]
 def deploy():
     with cd(PROJECT_DIR):
         run('git pull')
-        run('bin source/activate')
+        run('source flask_PSC/bin/activate')
         run('pip install -r requirements.txt')
         run('touch %s' % WSGI_SCRIPT)
 
